de60700484548bb4f1264cf632c01860fbe46529
David Morris <pixelgrapher@outlook.co.nz>
2020-03-17 17:11:37 +1300

    Dust in the air

--- Year3Proto3/Assets/Scripts/Effect/EnemyLight.cs
+++ Year3Proto3/Assets/Scripts/Effect/EnemyLight.cs
@@ -8,15 +8,35 @@ public class EnemyLight : MonoBehaviour
     private Light light;
     private float intensity;
     [Range(0, 1)] public float flickerAmount;
+    public float flickerRateMin = 10.0f;
+    public float flickerRateMax = 30.0f;
+    private float flickerTimer;
 
     void Start()
     {
         light = GetComponent<Light>();
         intensity = light.intensity;
+        flickerTimer = 1.0f / Random.Range(flickerRateMin, flickerRateMax);
     }
 
 
+    private void Update()
+    {
+        flickerTimer -= Time.deltaTime;
+
+        if (flickerTimer <= 0.0f)
+        {
+            SetFlicker();
+            flickerTimer = 1.0f / Random.Range(flickerRateMin, flickerRateMax);
+        }
+    }
+
     void FixedUpdate()
+    {
+
+    }
+
+    private void SetFlicker()
     {
         float flickerAdd = Random.Range(-intensity, intensity) * flickerAmount;
 

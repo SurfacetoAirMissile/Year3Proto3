4067ab0ce502d2aebeeafe5b289fac6286e1c10f
David Morris <pixelgrapher@outlook.co.nz>
2020-03-17 16:02:29 +1300

    Enemy light flicker

---
+++ Year3Proto3/Assets/Scripts/Effect/EnemyLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnemyLight : MonoBehaviour
+{
+    private Light light;
+    private float intensity;
+    [Range(0, 1)] public float flickerAmount;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        intensity = light.intensity;
+    }
+
+
+    void FixedUpdate()
+    {
+        float flickerAdd = Random.Range(-intensity, intensity) * flickerAmount;
+
+        light.intensity = intensity + flickerAdd;
+    }
+}

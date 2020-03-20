2b5b6efdd6890dabf3e7d3d9cb9cc6f096931ed0
iHusker <ihuskypuppy@gmail.com>
2020-03-21 11:18:05 +1300

    Door Update

---
+++ Year3Proto3/Assets/Scripts/General/Door.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Door : MonoBehaviour
+{
+    public DoorCollider doorCollider;
+
+    public Vector3 finalPosition;
+    private Vector3 initialPosition;
+
+    private bool open = false;
+    private bool interact = true;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && interact && doorCollider.HasEntered())
+        {
+            interact = false;
+
+            transform.DOKill(false);
+            transform.DOLocalMove((open) ? initialPosition : finalPosition, 1.2f)
+                .SetEase(Ease.OutQuint)
+                .OnComplete(() => {
+                    open = (open) ? false : true;
+                    interact = true;
+                });
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(finalPosition, transform.localScale);
+    }
+}

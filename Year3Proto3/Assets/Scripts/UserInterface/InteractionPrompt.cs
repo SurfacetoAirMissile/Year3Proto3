1f8e1b53a2bc21cdf8e25f03345b1bc1d445c8d0
David Morris <pixelgrapher@outlook.co.nz>
2020-03-30 17:57:50 +1300

    Interaction prompts

---
+++ Year3Proto3/Assets/Scripts/UserInterface/InteractionPrompt.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Interaction
+{
+    None,
+    Generic,
+    Hacking,
+    Pickup,
+    Holding,
+    Puzzle
+}
+
+public class InteractionPrompt : MonoBehaviour
+{
+    public Interaction interactionState;
+    private GameObject[] prompts;
+
+    void Start()
+    {
+        GetComponent<CanvasGroup>().alpha = 1.0f;
+
+        prompts = new GameObject[transform.childCount];
+
+        for (int i = 0; i < prompts.Length; i++)
+        {
+            prompts[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+
+    void Update()
+    {
+        for (int i = 0; i < prompts.Length; i++)
+        {
+            if (i == (int)interactionState) { prompts[i].SetActive(true); }
+            else { prompts[i].SetActive(false); }
+        }
+    }
+
+    public void SetPrompt(Interaction _interaction)
+    {
+        interactionState = _interaction;
+    }
+}
